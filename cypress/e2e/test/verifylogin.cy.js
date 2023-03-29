3c4578f117631fa37e407a2933738afa91a6dcae
SamikshyaS <samikshyasth123@gmail.com>
2023-03-29 21:42:52 +0545

    Cypress using pom

---
+++ cypress/e2e/test/verifylogin.cy.js
@@ -0,0 +1,23 @@
+import loginpage from "../../Pageobject/loginpage";
+describe('Verify the loginpage', () => {
+    beforeEach(function () {
+        cy.visit('https://www.saucedemo.com/')
+    })
+    it("Verify the login page", () => {
+        const loginobj = new loginpage();
+        loginobj.enterusername('standard_user')
+        loginobj.enterpassword('secret_sauce')
+        loginobj.clickbutton()
+        loginobj.pagetitle().should('eq', 'Sauce Labs Backpack');
+    })
+    it('User should be able to naviaget to the page', () => {
+        const loginobj = new loginpage();
+        loginobj.enterusername('standard_user')
+        loginobj.enterpassword('secret_sauce')
+        loginobj.clickbutton()
+        loginobj.get('div.inventory_item_name').click();
+        loginobj.pagetitle().should('eq', 'Sauce Labs Backpack');
+
+
+    })
+})
