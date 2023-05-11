201db59cb1ed42ec4dd9b497bf0c83887d021d9e
SamikshyaS <samikshyashrestha@lftechnology.com>
2023-05-11 21:26:12 +0545

    Added all the credentials in the fixtures and updated the code

--- cypress/e2e/test/verifylogin.cy.js
+++ cypress/e2e/test/verifylogin.cy.js
@@ -1,23 +1,30 @@
-import loginpage from "../../Pageobject/loginpage";
-describe('Verify the loginpage', () => {
+import loginpage from "../../Pageobject/loginelement";
+describe("Login", () => {
+    const loginobj = new loginpage()
     beforeEach(function () {
-        cy.visit('https://www.saucedemo.com/')
+        cy.visit('www.saucedemo.com')
     })
-    it("Verify the login page", () => {
-        const loginobj = new loginpage();
-        loginobj.enterusername('standard_user')
-        loginobj.enterpassword('secret_sauce')
+    it("Verify that user can login to the application as a normal user", () => {
+        cy.fixture('credentials').then((credentials) => {
+            cy.login(credentials.username1, credentials.password1)
+          })
+        loginobj.enterusername('username1')
+        loginobj.enterpassword('password1')
         loginobj.clickbutton()
-        loginobj.pagetitle().should('eq', 'Sauce Labs Backpack');
+        cy.title().should('eq', 'Sauce Labs Backpack')
     })
-    it('User should be able to naviaget to the page', () => {
-        const loginobj = new loginpage();
-        loginobj.enterusername('standard_user')
-        loginobj.enterpassword('secret_sauce')
+    it("Verify that user can navigate to the inventory page", () => {
+        cy.fixture('credentials').then((credentials) => {
+            cy.login(credentials.username1, credentials.password1)
+          })
+        loginobj.enterusername('username1')
+        loginobj.enterpassword('password1')
         loginobj.clickbutton()
-        loginobj.get('div.inventory_item_name').click();
-        loginobj.pagetitle().should('eq', 'Sauce Labs Backpack');
-
-        //this is the changes to create a pr ignore this
+        cy.get('div.inventory_item_name').click()
+        cy.title().should('eq', 'Sauce Labs Backpack')
     })
 })
+
+
+//create a function for the page title and format the test
+//try to store the credentials on fixtures
